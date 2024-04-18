2cd2dce7220155f8d6e558306bac8a55949acf74
drain shawty <hopegasgas@gmail.com>
2024-04-19 00:30:47 +0300

    zdravstvuyte

--- Lab2/src/test/java/com/drainshawty/lab2/ReflectionUtil.java
+++ Lab2/src/test/java/com/drainshawty/lab2/ReflectionUtil.java
@@ -6,14 +6,6 @@ import java.lang.reflect.Field;
 
 public class ReflectionUtil {
 
-    public static Object createInstance(Class<?> clazz, Class<?>[] parameterTypes, Object... args) throws Throwable {
-        try {
-            return clazz.getConstructor(parameterTypes).newInstance(args);
-        } catch (Exception ex) {
-            throw ex.getCause();
-        }
-    }
-
     public static void setField(Object object, String fieldName, Object value) {
         try {
             Field targetField = ReflectionUtils.findFields(
