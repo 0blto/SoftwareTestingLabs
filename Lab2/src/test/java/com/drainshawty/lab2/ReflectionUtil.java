87b3c09f08c0d0926c7b8df9ac873fbb557904c2
drain shawty <hopegasgas@gmail.com>
2024-04-19 10:28:40 +0300

    zdravstvuyte

--- Lab2/src/test/java/com/drainshawty/lab2/ReflectionUtil.java
+++ Lab2/src/test/java/com/drainshawty/lab2/ReflectionUtil.java
@@ -11,7 +11,7 @@ public class ReflectionUtil {
             Field targetField = ReflectionUtils.findFields(
                     object.getClass(),
                     field -> field.getName().equals(fieldName),
-                    ReflectionUtils.HierarchyTraversalMode.BOTTOM_UP
+                    ReflectionUtils.HierarchyTraversalMode.TOP_DOWN
             ).get(0);
             targetField.setAccessible(true);
             targetField.set(object, value);
