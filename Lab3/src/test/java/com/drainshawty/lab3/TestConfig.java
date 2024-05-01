3d289324e85777ad1fc96695f315af6f41f982cc
drain shawty <hopegasgas@gmail.com>
2024-05-01 21:53:21 +0300

    zdravstvuyte

--- Lab3/src/test/java/com/drainshawty/lab3/TestConfig.java
+++ Lab3/src/test/java/com/drainshawty/lab3/TestConfig.java
@@ -3,5 +3,5 @@ package com.drainshawty.lab3;
 public class TestConfig {
     public static String BASE_URI = "http://www.answers.com/";
     public static String USERNAME = "PODSHARIL";
-    public static String PASSWORD = "1qqqrzw1";
+    public static String PASSWORD = "НЕ СКАЖУ";
 }
