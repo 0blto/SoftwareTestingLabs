c040f034fd40c19156b1f60c345ce9fd6313803b
drain shawty <hopegasgas@gmail.com>
2024-05-02 02:12:50 +0300

    zdravstvuyte

--- Lab3/src/test/java/com/drainshawty/lab3/answers/ProfilePageTest.java
+++ Lab3/src/test/java/com/drainshawty/lab3/answers/ProfilePageTest.java
@@ -4,15 +4,12 @@ import com.drainshawty.lab3.AbstractPageTest;
 import com.drainshawty.lab3.TestConfig;
 import org.junit.jupiter.api.TestTemplate;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.net.URISyntaxException;
-import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class ProfilePageTest extends AbstractPageTest {
 
@@ -24,36 +21,30 @@ public class ProfilePageTest extends AbstractPageTest {
     }
 
     String getAbsolutePath(String fileName) throws URISyntaxException {
-        return new File(ProfilePage.class.getClassLoader().getResource(fileName).toURI()).getAbsolutePath();
+        return new File(getClass().getClassLoader().getResource(fileName).toURI()).getAbsolutePath();
     }
     @TestTemplate
     void imageChangeTest(WebDriver driver) throws URISyntaxException, InterruptedException {
         goToProfilePage(driver);
         ProfilePage profilePage = new ProfilePage(driver);
         profilePage.uploadPhoto(getAbsolutePath("before.jpg"));
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-        wait.until(visibilityOf(profilePage.getInfo()));
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         String beforeSrc = profilePage.getImgElement().getAttribute("src");
         profilePage.uploadPhoto(getAbsolutePath("after.jpg"));
-        wait.until(visibilityOf(profilePage.getInfo()));
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         assertNotEquals(beforeSrc, profilePage.getImgElement().getAttribute("src"), "Фотография не поменялась");
     }
 
     @TestTemplate
     void usernameChangeTest(WebDriver driver) throws InterruptedException {
         String tempName = "VIPERR";
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         goToProfilePage(driver);
         ProfilePage profilePage = new ProfilePage(driver);
         profilePage.changeUsername(tempName);
-        wait.until(visibilityOf(profilePage.getInfo()));
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         assertEquals(tempName, profilePage.getUsernameHeader().getText(), "Имя не удалось поменять");
         profilePage.changeUsername(TestConfig.USERNAME);
-        wait.until(visibilityOf(profilePage.getInfo()));
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         assertEquals(TestConfig.USERNAME, profilePage.getUsernameHeader().getText(), "Имя не удалось вернуть");
     }
 }
